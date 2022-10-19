b1c3a88c87955caa25ca0e3c936abaf4ebfc52b0
Isaiah <ooluwayomibrainard@yahoo.com>
2022-10-18 22:09:45 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,49 +1,31 @@
 #include <stdio.h>
 /**
-*main-Prints the first 98 Fibonacci numbers
+*main-Entry point
 *
-*Return:Always 0.
+*Description:program that finds and prints the first 98 Fibonacci
+*numbers, starting with 1 and 2
+*
+*Return:Always 0 successful
 */
 int main(void)
 {
-	int c, boolean, boolean2;
-	long int n1, n2, fn, fn2, n11, n22;
+	unsigned long int t0 = 0, t1 = 1, s;
 
-	n1 = 1;
-	n2 = 2;
-	boolean =  boolean2 = 1;
-	printf("%ld, %ld", n1, n2);
-	for (c = 0; c < 96, c++)
+	int i;
+
+	for (i = 1; i < = 98; i++)
 	{
-	if (boolean)
+	s = t0 + t1
+	if (i != 98)
 	{
-	fn = n1 + n2;
-	printf(", %ld", fn);
-	n1 = n2;
-	n2 = fn;
+	printf("%lu, ", s);
 	}
 	else
 	{
-	if (boolean2)
-	{
-	n11 = n1 % 1000000000;
-	n22 = n2 % 1000000000;
-	n1 = n1 / 1000000000;
-	n2 = n2 / 1000000000;
-	boolean2 = 0;
+	printf("%lu\n", s);
 	}
-	fn2 = (n11 + n22);
-	fn = n1 + n2 + (fn2 / 1000000000);
-	printf(", %ld", fn);
-	printf("%ld", fn2 % 1000000000);
-	n1 = n2;
-	n11 = n22;
-	n2 = fn;
-	n22 = (fn2 % 1000000000);
+	t0 = t1;
+	t1 = s;
 	}
-	if (((n1 + n2) < 0) && boolean == 1)
-	boolean = 0;
-	}
-	printf("\n");
 	return (0);
 }
