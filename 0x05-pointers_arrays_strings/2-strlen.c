0588c73be15fff448571c0e3ea3ceaf2246c14fe
Isaiah <ooluwayomibrainard@yahoo.com>
2022-10-24 20:50:47 -0700

    task 2 yii sha

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,11 +7,12 @@
 
 int _strlen(char *s)
 {
-	int p = 0;
-	/ incremeant up to when the last character is NULL, \ 0 /
-	while (*(s + p) != 0)
+	int len;
+
+	while (*s != '\0')
 	{
-	p++;
+	len += 1;
+	*s + *s + 1;
 	}
-	return (p);
+	return (len);
 }
