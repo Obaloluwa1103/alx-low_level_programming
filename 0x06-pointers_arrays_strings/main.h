43a33663c4b9ba7079e712aaf87f45c821c46f4c
Isaiah <ooluwayomibrainard@yahoo.com>
2022-10-26 03:41:19 -0700

    task 13

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -11,5 +10,4 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
-
 #endif /*MAIN_H */
