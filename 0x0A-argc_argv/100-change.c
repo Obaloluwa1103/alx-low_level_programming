86aa43ae5bdaa2daa4a69a6c808a74b9a8fe9305
Isaiah <ooluwayomibrainard@yahoo.com>
2022-11-04 17:39:24 -0700

    task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,42 +1,39 @@
-Advance task 5
-
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- *  * main - Entry point
- *   * @argc: arg count
- *    * @argv: arg vector
- *     * Return: success
- *      */
+*main-Entry point
+*@argc:arg count
+*@argv:arg vector
+*Return:success
+*/
 
 int main(int argc, char *argv[])
 {
-		int change[] = {25, 10, 5, 2, 1};
-			int a, b, i, num, result = 0;
+	int change[] = {25, 10, 5, 2, 1};
+	int a, b, i, num, result = 0;
 
-				if (argc <= 1)
-						{
-									printf("Error\n");
-											return (1);
-												}
+	if (argc <= 1)
+	{
+	printf("Error\n");
+	return (1);
+	}
 
-					num = atoi(argv[1]);
+	num = atoi(argv[1]);
 
-						if (num < 0)
-									printf("0\n");
-							else
-									{
-												for (i = 0; i < 5; i++)
-															{
-																			a = num / change[i];
-																						b = num % change[i];
-																									result += a;
-																												num = b;
-																														}
+	if (num < 0)
+	printf("0\n");
+	else
+	{
+	for (i = 0; i < 5; i++)
+	{
+	a = num / change[i];
+	b = num % change[i];
+	result += a;
+	num = b;
+	}
 
-														printf("%d\n", result);
-															}
+	printf("%d\n", result);
+	}
 
-								exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
