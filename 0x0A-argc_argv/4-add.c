d6e52a77207fa039264fa7274185f636a92afd2f
Isaiah <ooluwayomibrainard@yahoo.com>
2022-11-04 17:27:58 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,7 +6,7 @@ int is_number(char *str);
 *@argc: arg count
 *@argv:arg vector
 *Return: success
- *      */
+*/
 
 int main(int argc, char *argv[])
 {
@@ -16,43 +16,41 @@ int main(int argc, char *argv[])
 	{
 	for (i = 1; i < argc; i++)
 	{
-															if (is_number(argv[i]) == 1)
-																			{
-																								printf("Error\n");
-																												return (1);
-																															}
-																		else
-																							result += strtol(argv[i], NULL, 10);
-																				}
-										printf("%d\n", result);
-											}
-				else
-						{
-									printf("0\n");
-											return (0);
-												}
+	if (is_number(argv[i]) == 1)
+	{
+	printf("Error\n");
+	return (1);
+	}
+	else
+	result += strtol(argv[i], NULL, 10);
+	}
+	printf("%d\n", result);
+	}
+	else
+	{
+	printf("0\n");
+	return (0);
+	}
 
-					exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
-
 /**
- *  * is_number - checks if a character is a number
- *   * @str: param to check
- *    * Return: 0 if true else 1
- *     */
+*is_number-checks if a character is a number
+*@str:param to check
+*Return:0 if true else 1
+*/
 
 int is_number(char *str)
 {
-		int i = 0;
+	int i = 0;
 
-			if (str[0] == '-')
-						i = 1;
+	if (str[0] == '-')
+	i = 1;
+	for (; str[i]; i++)
+	{
+	if (*(str + i) < '0' || *(str + i) > '9')
+	return (1);
+	}
 
-				for (; str[i]; i++)
-						{
-									if (*(str + i) < '0' || *(str + i) > '9')
-													return (1);
-										}
-
-					return (0);
+	return (0);
 }
