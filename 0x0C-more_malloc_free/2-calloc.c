40eb90a367acac2d1bed78e2d0b654bf762813b2
Isaiah <ooluwayomibrainard@yahoo.com>
2022-11-10 07:12:39 -0800

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,28 +1,34 @@
 #include "main.h"
-#include <stdlib.h>
 /**
-*calloc-allocates memory for an array
-*@nmemb:number of elements in an array
-*@size:size of elements in array
-*Return:pointer to allocated memory or NULL otherwise
+*_calloc-allocates memory for an array, using malloc
+*@nmemb:number of elements of pointer
+*@size:size of each member
+*Return:pointer of allocated memory
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
-	unsigned int i;
+	void *ptr;
 
-	if (nmemb == 0 || size == 0)
+	if (!nmemb || !size)
 	return (NULL);
-	p = malloc(nmemb * size);
-	if (p == NULL)
-	{
+	ptr = malloc(size * nmemb);
+	if (!ptr)
 	return (NULL);
-	}
+	_memset(ptr, 0, size * nmemb);
+	return (ptr);
+}
+/**
+*_memset-fills memory with a constant byte
+*@s:memory area
+*@b:constant byte
+*@n:bytes of the memory area
+*Return:pointer to the memory area s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
 
-	for (i = 0; i < (nmemb * size); i++)
-	{
-	*((char *)(p) + i) = 0;
-	}
-
-	return (p);
+	while (n--)
+	*s++ = b;
+	return (ptr);
 }
